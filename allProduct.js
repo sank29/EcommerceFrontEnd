e5593d898e6d7c8d2a369d347bba7e10635bc143
sanket <wankhdes29@gmail.com>
2023-01-19 10:33:45 +0530

    displaying all product on product page

--- allProduct.js
+++ allProduct.js
@@ -1,5 +1,7 @@
 let uuid = localStorage.getItem("uuidkey");
 
+let allProduct = document.getElementById("allProduct");
+
 let getAllProudct = async () => {
   let url = `http://localhost:8888/products?key=${uuid}`;
 
@@ -12,4 +14,35 @@ let getAllProudct = async () => {
   return data1;
 };
 
-getAllProudct();
+let displayAllProduct = async () => {
+  let productData = await getAllProudct();
+
+  let newDiv = document.createElement("div");
+
+  productData.forEach((eachProuct) => {
+    let para1 = document.createElement("p");
+    let para2 = document.createElement("p");
+    let para3 = document.createElement("p");
+    let para4 = document.createElement("p");
+    let para5 = document.createElement("p");
+
+    let img = document.createElement("img");
+    img.setAttribute("class", "productImg");
+    img.style.width = "200px";
+
+    // img.
+    img.src = eachProuct.productImg;
+
+    para4.append(img);
+
+    para1.innerText = eachProuct.productName;
+
+    para2.innerText = eachProuct.quantity;
+
+    para3.innerText = eachProuct.type;
+
+    allProduct.append(para4, para1, para2, para3);
+  });
+};
+
+displayAllProduct();
